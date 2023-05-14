97d5ee52338921082177bca0204fc021d0d5ff1d
Nick Nazarov <justmove8@gmail.com>
2023-05-14 23:13:49 +0300

    02 add buttons

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="panel">
+    <app-button sex="male" :type="maleActive" @action="active = 'male'"></app-button>
+    <app-button sex="female" :type="femaleActive" @action="active = 'female'"></app-button>
+  </div>
+</template>
+
+<script>
+import AppButton from '@/AppButton'
+
+export default {
+  data () {
+    return {
+      active: 'male'
+    }
+  },
+  methods: {
+  },
+  computed: {
+    maleActive () {
+      return this.active === 'male' ? 'active' : ''
+    },
+    femaleActive () {
+      return this.active === 'female' ? 'active' : ''
+    }
+  },
+  components: {
+    'app-button': AppButton
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/AppButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="btn" :class="type" @click="$emit('action')">
+    <img :class="getClass" :src="require('./img/' + getIcon)" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['action'],
+  props: {
+    type: String,
+    sex: String
+  },
+  mounted () {
+    console.log(this.sex)
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+    getIcon () {
+      return this.sex === 'female' ? 'femaleicon.png' : 'maleicon.png'
+    },
+    getClass () {
+      return this.sex === 'female' ? 'female' : 'male'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
